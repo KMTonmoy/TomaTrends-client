68182d95c9d737af2cabcce54591ae36776b86ac
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-09-25 03:39:07 +0600

    Banner added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from '../Components/Header/Banner';
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is HomePage</h1>
+        <div className='min-h-screen max-w-7xl mx-auto'>
+            <Banner />
         </div>
     );
 };
